b5b84aab6da84548253da7cbc706a93fb60563de
Aljina12 <aljinarai105@gmail.com>
2025-07-11 10:57:31 +0545

    Crud complete

---
+++ HRMS/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import LoginForm from './pages/login';
+import Home from './pages/home';
+import EmployeeForm from './pages/employee';
+import DashboardForm from './pages/dashboard';
+
+function App() {
+ 
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginForm/>} />
+        <Route path="Home" element={<Home />}>
+         <Route path="employee" element={<EmployeeForm />} />
+         <Route path="dashboard" element={<DashboardForm />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
