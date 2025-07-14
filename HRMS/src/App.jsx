b4a63ec45fa6b730d61df49e84b06a5c1c7eb53a
Aljina12 <aljinarai105@gmail.com>
2025-07-14 10:24:02 +0545

    Success

--- HRMS/src/App.jsx
+++ HRMS/src/App.jsx
@@ -22,7 +22,13 @@ function App() {
         </ProtectedRoute>
         }
         >
-         <Route path="employee" element={<EmployeeForm />} />
+         <Route path="employee" 
+         element={
+        <ProtectedRoute allowedRoles={["hr", "manager", "admin"]}>
+         <EmployeeForm />
+        </ProtectedRoute>
+        } 
+        />
          <Route path="dashboard" element={<DashboardForm />} />
         </Route>
       </Routes>
